42a14dd81a3d5cf3e48f61f7da2bb06bc47e980d
Miku Sano <miku39.sano@gmail.com>
2023-01-22 21:21:56 +0900

    solved: happy number

---
+++ ProgrammingSkills1/202.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(isHappy(19))
+}
+
+// 202. Happy Number
+func isHappy(n int) bool {
+	previous := []int{n}
+
+	for {
+		n = calc(n)
+		if n == 1 {
+			return true
+		}
+		if isDuplicate(n, previous) {
+			return false
+		}
+		previous = append(previous, n)
+	}
+}
+
+func calc(num int) int {
+	sum := 0
+	for num != 0 {
+		digit := num % 10
+		sum += digit * digit
+		num = num / 10
+	}
+	return sum
+}
+
+func isDuplicate(num int, previous []int) bool {
+	for _, v := range previous {
+		if v == num {
+			return true
+		}
+	}
+	return false
+}
